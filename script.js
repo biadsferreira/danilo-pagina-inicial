5df19355107363290c430e5ceab8fc41b394f255
Beatriz Ferreia <biadsferreira@gmail.com>
2023-06-23 18:50:15 -0300

    primeiro commit pagina inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+function clickMenu() {
+    if (menuMobile.style.display == 'none') {
+        menuMobile.style.display = 'flex'
+    }
+    else {
+        menuMobile.style.display = 'none'
+    }
+}
+
+function clickMenuMobile() {
+    if (menuMenor.style.display == 'none') {
+        menuMenor.style.display = 'flex'
+    }
+    else {
+        menuMenor.style.display = 'none'
+    }
+}
